b9c7cbd2cdb8fb82788d4fc2db5eaeb4d8dfffb1
Vasylyna Khasanova <wassilissa.ws@gmail.com>
2025-02-24 23:10:01 +0200

    check URL methods were added

--- src/main/java/org/pages/LoginPage.java
+++ src/main/java/org/pages/LoginPage.java
@@ -1,5 +1,24 @@
 package org.pages;
 
-public class LoginPage {
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends ParentPage {
+    private Logger logger = Logger.getLogger(getClass());
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    protected String getRelativeUrl() {
+        return "/login";
+    }
+
+    public LoginPage openPage() {
+        webDriver.get(baseUrl);
+        logger.info("Login Page was opened with url " + baseUrl);
+        return this;
+    }
 
 }
